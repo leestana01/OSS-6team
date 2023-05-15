507fd7e449d4502529d7bd110a318f785cf5f0ab
leestana01 <leestana01@naver.com>
2023-05-15 18:38:15 +0900

    최초 공유 세팅

---
+++ app/src/main/java/com/oss6team/emptylecturefinder/CheckBuilding.kt
@@ -0,0 +1,11 @@
+package com.oss6team.emptylecturefinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckBuilding : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_building)
+    }
+}
---
+++ app/src/main/java/com/oss6team/emptylecturefinder/CheckTime.kt
@@ -0,0 +1,11 @@
+package com.oss6team.emptylecturefinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckTime : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_time)
+    }
+}
