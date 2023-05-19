9582402154f30c94314e20869df1666b2a6d1515
heeyupp <130219670+heeyupp@users.noreply.github.com>
2023-05-19 18:46:01 +0900

    시간대 구현중
    해보다가 버스시간되서 집가서 마저 하겠습니다ㅎㅎ..

--- app/src/main/java/com/oss6team/emptylecturefinder/CheckTime.kt
+++ app/src/main/java/com/oss6team/emptylecturefinder/CheckTime.kt
@@ -1,11 +1,31 @@
 package com.oss6team.emptylecturefinder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.professionalandrioid.apps.timeapp.R
+
 
 class CheckTime : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_time)
+
+        val class1_checkBox = findViewById<View>(R.id.checkbox_1st_class) as CheckBox
+        val class2_checkBox = findViewById<View>(R.id.checkbox_2nd_class) as CheckBox
+        val class3_checkBox = findViewById<View>(R.id.checkbox_3rd_class) as CheckBox
+        val class4_checkBox = findViewById<View>(R.id.checkbox_4th_class) as CheckBox
+        val class5_checkBox = findViewById<View>(R.id.checkbox_5th_class) as CheckBox
+        val class6_checkBox = findViewById<View>(R.id.checkbox_6th_class) as CheckBox
+        val class7_checkBox = findViewById<View>(R.id.checkbox_7th_class) as CheckBox
+        val class8_checkBox = findViewById<View>(R.id.checkbox_8th_class) as CheckBox
+        val class9_checkBox = findViewById<View>(R.id.checkbox_9th_class) as CheckBox
+
+
+        if (class1_checkBox.isChecked){
+            Toast.makeText(this,"1교시 선택됨",Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/oss6team/emptylecturefinder/MainActivity.kt
+++ app/src/main/java/com/oss6team/emptylecturefinder/MainActivity.kt
@@ -3,11 +3,12 @@ package com.oss6team.emptylecturefinder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import com.professionalandrioid.apps.timeapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) //다크모드 비활성화
-        setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.activity_check_time)
     }
 }
