4bd00336bc428b5200a13e939f130ae086703648
cheolm <ninjababo12@naver.com>
2023-05-24 14:58:44 +0900

    변수작업완료

--- app/src/main/java/com/oss6team/emptylecturefinder/Seoul_building.kt
+++ app/src/main/java/com/oss6team/emptylecturefinder/Seoul_building.kt
@@ -1,11 +1,75 @@
 package com.oss6team.emptylecturefinder
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Seoul_building : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_building)
+
+        val btn_seoul_town1 = findViewById<View>(R.id.btn_seoul_town1) as Button
+        btn_seoul_town1.setOnClickListener {
+            val campus=0;
+            val building=1;
+
+            val intent = Intent(this@Seoul_building, LectureFound::class.java)
+            intent.putExtra("campus", campus)
+            intent.putExtra("building", building)
+            startActivity(intent)
+        }
+        val btn_seoul_town2 = findViewById<View>(R.id.btn_seoul_town2) as Button
+        btn_seoul_town2.setOnClickListener {
+            val campus=0;
+            val building=2;
+
+            val intent = Intent(this@Seoul_building, LectureFound::class.java)
+            intent.putExtra("campus", campus)
+            intent.putExtra("building", building)
+            startActivity(intent)
+        }
+        val btn_seoul_town3 = findViewById<View>(R.id.btn_seoul_town3) as Button
+        btn_seoul_town3.setOnClickListener {
+            val campus=0;
+            val building=3;
+
+            val intent = Intent(this@Seoul_building, LectureFound::class.java)
+            intent.putExtra("campus", campus)
+            intent.putExtra("building", building)
+            startActivity(intent)
+        }
+        val btn_seoul_town4 = findViewById<View>(R.id.btn_seoul_town4) as Button
+        btn_seoul_town4.setOnClickListener {
+            val campus=0;
+            val building=4;
+
+            val intent = Intent(this@Seoul_building, LectureFound::class.java)
+            intent.putExtra("campus", campus)
+            intent.putExtra("building", building)
+            startActivity(intent)
+        }
+        val btn_seoul_town5 = findViewById<View>(R.id.btn_seoul_town5) as Button
+        btn_seoul_town5.setOnClickListener {
+            val campus=0;
+            val building=5;
+
+            val intent = Intent(this@Seoul_building, LectureFound::class.java)
+            intent.putExtra("campus", campus)
+            intent.putExtra("building", building)
+            startActivity(intent)
+        }
+        val btn_seoul_town6 = findViewById<View>(R.id.btn_seoul_town6) as Button
+        btn_seoul_town6.setOnClickListener {
+            val campus=0;
+            val building=6;
+
+            val intent = Intent(this@Seoul_building, LectureFound::class.java)
+            intent.putExtra("campus", campus)
+            intent.putExtra("building", building)
+            startActivity(intent)
+        }
     }
 }
