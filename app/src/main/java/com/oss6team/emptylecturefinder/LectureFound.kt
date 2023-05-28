2a1c7ccc3693b2e667614f7f10115aa3bf0c16ef
heeyupp <130219670+heeyupp@users.noreply.github.com>
2023-05-29 03:17:53 +0900

    배경 추가와 글씨들을 정렬했습니다. 다만 출력화면의 크기를 좀 줄여서 "원하는 강의실을 찾았어요!" 를 넣고 싶었는데, 어떻게 줄이는지 못찾았습니다.. 배경색들은 임의로 해놓은것들이라 언제든 바꿀수 있습니다

--- app/src/main/java/com/oss6team/emptylecturefinder/LectureFound.kt
+++ app/src/main/java/com/oss6team/emptylecturefinder/LectureFound.kt
@@ -1,7 +1,10 @@
 package com.oss6team.emptylecturefinder
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
@@ -53,9 +56,9 @@ class LectureFound : AppCompatActivity() {
             filteredByBuilding
         }
 
-
         displayDataInTable(filteredByLecture)
     }
+
 
     private fun readDataFromFile(resId: Int): List<Classroom> {
         val inputStream = resources.openRawResource(resId)
@@ -78,27 +81,79 @@ class LectureFound : AppCompatActivity() {
     }
 
     private fun displayDataInTable(data: List<Classroom>) {
+        val headerRow = createHeaderRow()
+        tableLayout.addView(headerRow)
+
         data.forEach { classroom ->
-            if (classroom.times.isNotEmpty()) { // 추가된 부분
-                val row = TableRow(this)
-                val layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT)
-                row.layoutParams = layoutParams
-
-                val textViewClassroom = TextView(this)
-                textViewClassroom.text = classroom.classroom
-                row.addView(textViewClassroom)
-
-                val textViewDay = TextView(this)
-                textViewDay.text = classroom.day
-                row.addView(textViewDay)
-
-                val textViewTimes = TextView(this)
-                textViewTimes.text = classroom.times.joinToString(", ")
-                row.addView(textViewTimes)
-
+            if (classroom.times.isNotEmpty()) {
+                val row = createDataRow(classroom)
                 tableLayout.addView(row)
             }
         }
+    }
+
+    private fun createHeaderRow(): TableRow {
+        val headerRow = TableRow(this)
+        val layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT)
+        headerRow.layoutParams = layoutParams
+
+        val columnHeaderClassroom = createTableHeaderTextView("Classroom")
+        headerRow.addView(columnHeaderClassroom)
+
+        val columnHeaderDay = createTableHeaderTextView("Day")
+        headerRow.addView(columnHeaderDay)
+
+        val columnHeaderTimes = createTableHeaderTextView("Times")
+        headerRow.addView(columnHeaderTimes)
+
+        return headerRow
+    }
+
+    private fun createTableHeaderTextView(text: String): TextView {
+        val textView = TextView(this)
+        textView.text = text
+        textView.setPadding(5.dpToPx(), 5.dpToPx(), 5.dpToPx(), 5.dpToPx())
+        textView.setTextColor(Color.WHITE)
+        textView.setTypeface(null, Typeface.BOLD)
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP,20.0f)
+        val layoutParams = TableRow.LayoutParams(
+            TableRow.LayoutParams.WRAP_CONTENT,
+            TableRow.LayoutParams.WRAP_CONTENT
+        )
+        layoutParams.setMargins(2.dpToPx(), 2.dpToPx(), 2.dpToPx(), 2.dpToPx())
+        textView.layoutParams = layoutParams
+
+        textView.setBackgroundResource(R.drawable.cell_background)
+
+        return textView
+    }
+
+    private fun createDataRow(classroom: Classroom): TableRow {
+        val row = TableRow(this)
+        val layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT)
+        row.layoutParams = layoutParams
+
+        val textViewClassroom = createDataTextView(classroom.classroom)
+        row.addView(textViewClassroom)
+
+        val textViewDay = createDataTextView(classroom.day)
+        row.addView(textViewDay)
+
+        val textViewTimes = createDataTextView(classroom.times.joinToString(", "))
+        row.addView(textViewTimes)
+
+        return row
+    }
+
+    private fun createDataTextView(text: String): TextView {
+        val textView = TextView(this)
+        textView.text = text
+        textView.setPadding(5.dpToPx(), 5.dpToPx(), 5.dpToPx(), 5.dpToPx())
+        return textView
+    }
+
+    private fun Int.dpToPx(): Int {
+        return (this * resources.displayMetrics.density).toInt()
     }
 
 
