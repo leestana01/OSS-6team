d563310b3de47b2d8179a842e14c66cc63471566
leestana01 <leestana01@naver.com>
2023-05-30 20:58:32 +0900

    즐겨찾기 기능 추가

--- app/src/main/java/com/oss6team/emptylecturefinder/Favorites.kt
+++ app/src/main/java/com/oss6team/emptylecturefinder/Favorites.kt
@@ -1,78 +1,43 @@
 package com.oss6team.emptylecturefinder
 
-import android.content.Intent
-import android.graphics.Color
-import android.graphics.Typeface
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.ContextThemeWrapper
-import android.view.Gravity
+import android.view.View
 import android.widget.*
-import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStreamReader
 
-
-class LectureFound : AppCompatActivity() {
+class Favorites : AppCompatActivity() {
 
     private lateinit var tableLayout: TableLayout
+    private lateinit var deleteButton: Button
+    private val checkboxMap = mutableMapOf<CheckBox, LectureFound.Classroom>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lecture_found)
+        setContentView(R.layout.activity_favorites)
 
         tableLayout = findViewById(R.id.tableLayout)
+        deleteButton = findViewById(R.id.deleteButton)
 
-        val saveButton: Button = findViewById(R.id.saveButton)
-        saveButton.setOnClickListener {
-            saveCheckedItems()
+        val file = File(getExternalFilesDir(null), "favorites.txt")
+        val data = readDataFromFile(file)
+
+        deleteButton.setOnClickListener {
+            deleteCheckedRows()
         }
 
-        val campus = intent.getStringExtra("campus")
-        val building = intent.getStringExtra("building")
-        val lecture = intent.getIntegerArrayListExtra("lecture") ?: arrayListOf()
-
-        val fileResId = if (campus == "0") R.raw.seoul_remaining else R.raw.global_remaining
-
-        val data = readDataFromFile(fileResId)
-
-        val filteredByBuilding = if (building != null) {
-            data.filter { it.classroom.startsWith(building.toString()) }
-        } else {
-            data
-        }
-
-        val filteredByLecture = if (lecture.isNotEmpty()) {
-            val checkedTimes = lecture.mapIndexed { index, isChecked ->
-                if (isChecked == 1) index + 1 else null
-            }.filterNotNull()
-
-            val andOr = intent.getIntExtra("andOr", 1)
-
-            if (andOr == 0) {
-                filteredByBuilding.filter { classroom ->
-                    checkedTimes.all { time -> classroom.times.contains(time) }
-                }
-            } else {
-                filteredByBuilding.filter { classroom ->
-                    checkedTimes.any { time -> classroom.times.contains(time) }
-                }
-            }
-        } else {
-            filteredByBuilding
-        }
-
-        displayDataInTable(filteredByLecture)
+        displayDataInTable(data)
     }
 
-
-    private fun readDataFromFile(resId: Int): List<Classroom> {
-        val inputStream = resources.openRawResource(resId)
+    private fun readDataFromFile(file: File): List<LectureFound.Classroom> {
+        val inputStream = file.inputStream()
         val reader = BufferedReader(InputStreamReader(inputStream))
-        val data = mutableListOf<Classroom>()
+        val data = mutableListOf<LectureFound.Classroom>()
 
         reader.useLines { lines ->
             lines.forEach {
@@ -81,7 +46,7 @@ class LectureFound : AppCompatActivity() {
                     val classroom = parts[0]
                     val day = parts[1]
                     val times = if (parts[2].isNotEmpty()) parts[2].split(" ").map { it.toInt() } else listOf()
-                    data.add(Classroom(classroom, day, times))
+                    data.add(LectureFound.Classroom(classroom, day, times))
                 }
             }
         }
@@ -89,7 +54,7 @@ class LectureFound : AppCompatActivity() {
         return data
     }
 
-    private fun displayDataInTable(data: List<Classroom>) {
+    private fun displayDataInTable(data: List<LectureFound.Classroom>) {
         val headerRow = createHeaderRow()
         tableLayout.addView(headerRow)
 
@@ -97,6 +62,7 @@ class LectureFound : AppCompatActivity() {
             if (classroom.times.isNotEmpty()) {
                 val row = createDataRow(classroom)
                 tableLayout.addView(row)
+                checkboxMap[row.getChildAt(0) as CheckBox] = classroom
             }
         }
     }
@@ -134,7 +100,7 @@ class LectureFound : AppCompatActivity() {
         return textView
     }
 
-    private fun createDataRow(classroom: Classroom): TableRow {
+    private fun createDataRow(classroom: LectureFound.Classroom): TableRow {
         val contextThemeWrapper = ContextThemeWrapper(this, R.style.TableRowStyle)
 
         val row = TableRow(contextThemeWrapper)
@@ -164,42 +130,31 @@ class LectureFound : AppCompatActivity() {
         return textView
     }
 
-    private fun saveCheckedItems() {
-        val dataToSave = mutableListOf<Classroom>()
-
-        // Traverse table rows
-        for (i in 0 until tableLayout.childCount) {
-            val row = tableLayout.getChildAt(i) as? TableRow
-            val checkbox = row?.getChildAt(0) as? CheckBox // assuming checkbox is the first child
-            if (checkbox?.isChecked == true) {
-                // Get data from other views in the row and create Classroom object
-                val classroomName = (row.getChildAt(1) as? TextView)?.text.toString()
-                val classroomDay = (row.getChildAt(2) as? TextView)?.text.toString()
-                val classroomTimes = (row.getChildAt(3) as? TextView)?.text.toString()
-                    .split(", ").map { it.toInt() }
-                dataToSave.add(Classroom(classroomName, classroomDay, classroomTimes))
-            }
-        }
-
-        // Save data to file
-        saveDataToFile(dataToSave)
-
-        // Show toast message
-        Toast.makeText(this, R.string.Save_Complete, Toast.LENGTH_SHORT).show()
-
-        // Launch new activity
-        val intent = Intent(this, Favorites::class.java)
-        startActivity(intent)
-    }
-
-    private fun saveDataToFile(data: List<Classroom>) {
-        val file = File(getExternalFilesDir(null), "favorites.txt")
-        file.writeText(data.joinToString("\n") { "${it.classroom}, ${it.day}, ${it.times.joinToString(" ")}" })
-    }
-
     private fun Int.dpToPx(): Int {
         return (this * resources.displayMetrics.density).toInt()
     }
 
-    data class Classroom(val classroom: String, val day: String, val times: List<Int>)
+    private fun deleteCheckedRows() {
+        val classroomsToRemove = mutableListOf<LectureFound.Classroom>()
+        for ((checkbox, classroom) in checkboxMap) {
+            if (checkbox.isChecked) {
+                tableLayout.removeView(checkbox.parent as View)
+                classroomsToRemove.add(classroom)
+            }
+        }
+
+        removeClassroomsFromFile(classroomsToRemove)
+        classroomsToRemove.forEach { checkboxMap.values.remove(it) }
+
+        Toast.makeText(this, R.string.Delete_Complete, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun removeClassroomsFromFile(classroomsToRemove: List<LectureFound.Classroom>) {
+        val file = File(getExternalFilesDir(null), "favorites.txt")
+        val remainingClassrooms = readDataFromFile(file).filterNot { it in classroomsToRemove }
+        file.writeText(remainingClassrooms.joinToString("\n") {
+            "${it.classroom}, ${it.day}, ${it.times.joinToString(" ")}"
+        })
+    }
+
 }
